6607d4448fe2e982c61ea27d51dac4d56388f559
Govind kumar verma <govindkumar1389@gmail.com>
2026-05-31 14:52:45 +0530

    updating
    Signed-off-by: Govind kumar verma <govindkumar1389@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
     <div className="fixed items-center top-20 justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] text-4xl
     text-green-900">
      SHUBHAM SHIKHAR JEWELLERS
-     <br />
-     Updeted webs
+     <br className="text-center" />
+     Updeting progress this website
     </div>
 
       <div>
